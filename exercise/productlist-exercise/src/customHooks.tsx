81826daf7a037e872cf4d9f55ee511b56704de8b
Pulkit <pulkit.dadheech@noovosoft.com>
2023-10-17 17:29:52 +0530

    Remove unnecessary use of data variable in customHooks.tsx

--- exercise/productlist-exercise/src/customHooks.tsx
+++ exercise/productlist-exercise/src/customHooks.tsx
@@ -1,32 +1,23 @@
-import {useEffect, useMemo, useState} from 'react';
 import './productlist.css'
 import useFetch, {apiQueries, baseURL, createApiUrl} from "./dataFetchingFile";
 import {ProductCatalog, UserData} from "./dataTypes";
 
 export function useProductList(search?: string, category?: string) {
-    const [productCatalog, setProductCatalog] = useState<ProductCatalog>({
-        products: [],
-        total: 0,
-        skip: 0,
-        limit: 30,
-    }as ProductCatalog);
-
     let url: string;
 
     if (search) {
-        url = createApiUrl(apiQueries.Search,search);
+        url = createApiUrl(apiQueries.Search, search);
     } else if (category) {
-        url = createApiUrl(apiQueries.Category,category);
+        url = createApiUrl(apiQueries.Category, category);
     } else {
         url = createApiUrl(apiQueries.Product);
     }
-    let data: ProductCatalog;
-    data = useFetch<ProductCatalog>(url);
+    let data = useFetch<ProductCatalog>(url);
 
     if (search && category) {
         const newData = data.products.filter((product) => product.category === category)
-        if (data && newData) {
-            data = {
+        if (newData) {
+            return {
                 products: newData,
                 total: data.total,
                 skip: data.skip,
@@ -36,24 +27,13 @@ export function useProductList(search?: string, category?: string) {
         }
     }
 
-
-    useEffect(() => {
-        setProductCatalog(data);
-    }, [data]);
-    return useMemo(() => productCatalog, [productCatalog]);
+    return data;
 
 }
 
 export function useCategoryList() {
-    const [category, setCategory] = useState<string[]>([] as string[]);
     const url = `${baseURL}/products/categories`;
-    const data = useFetch<string[]>(url);
-
-    useEffect(() => {
-        setCategory(data);
-    }, [data]);
-
-    return useMemo(() => category, [category]);
+    return useFetch<string[]>(url);
 }
 
 
