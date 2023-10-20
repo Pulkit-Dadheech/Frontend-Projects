1e5c57d74c079f08e4d7a03d84febb40ecdbbff3
Pulkit <pulkit.dadheech@noovosoft.com>
2023-10-20 14:14:12 +0530

    Update Css for Pagination Buttons

--- exercise/productlist-exercise/src/components/Pagination/paginate.tsx
+++ exercise/productlist-exercise/src/components/Pagination/paginate.tsx
@@ -17,14 +17,14 @@ export default function Paginate({totalProducts, currentPage, setCurrentPage}: {
             {totalProducts ? (<div className="pagination">
                 <span className="pagination-button-block">
                     <button className={currentPage !== 1 ? "active" : ""}
-                            onClick={() => currentPage !== 1 && setCurrentPage(1)}>First</button>
+                            onClick={() => currentPage !== 1 && setCurrentPage(1)}>{"<<"}</button>
                     <button className={currentPage !== 1 ? "active" : ""}
                             onClick={() => setCurrentPage(currentPage !== 1 ? currentPage - 1 : 1)}>Prev</button>
                     <button className="active">{currentPage}</button>
                     <button className={currentPage !== totalPages ? "active" : ""}
                             onClick={() => setCurrentPage(currentPage !== totalPages ? currentPage + 1 : totalPages)}>Next</button>
                     <button className={currentPage !== totalPages ? "active" : ""}
-                            onClick={() => currentPage !== totalPages && setCurrentPage(totalPages)}>Last</button>
+                            onClick={() => currentPage !== totalPages && setCurrentPage(totalPages)}>{">>"}</button>
                 </span>
                     <div>Pages {currentPage} out of {totalPages}</div>
                 </div>) :
