d204f1e56cfd7eb8ed3ac0adc222b03bcf9443a7
Pulkit <pulkit.dadheech@noovosoft.com>
2023-10-26 15:54:58 +0530

    Refactor Product Form

--- exercise/productlist-exercise/src/customProduct/CustomProductsForm.tsx
+++ exercise/productlist-exercise/src/customProduct/CustomProductsForm.tsx
@@ -26,8 +26,13 @@ function ProductForm() {
     });
     const [successMessage, setSuccessMessage] = useState<string>('');
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const {name, value} = e.target;
+        if(name === 'discountPercentage'){
+            if(parseInt(value) >100 || parseInt(value) <0){
+                return
+            }
+        }
         setProductData({
             ...productData,
             [name]: value,
@@ -57,38 +62,43 @@ function ProductForm() {
 
     return (
         <div className="form-container">
-            <h2 className="form-header">Add a Custom Product</h2>
-            <form className="product-form">
-
+            <div className={"form-header"}>
+                <h2 className="form-header-text">Add a Custom Product</h2>
+                <Link to="/custom-product">View Custom Products</Link>
+            </div>
+            <form className="product-form" onSubmit={handleSave}>
                 <label htmlFor="name" className="input-label">Product Name:</label>
                 <input className="input-field" type="text" name="title" onChange={(e) => handleChange(e)}
-                       value={productData.title}/>
+                       value={productData.title} required />
 
                 <label htmlFor="category" className="input-label">Category:</label>
-                <input type="text" name="category" value={productData.category} onChange={handleChange}
-                       className="input-field"/>
+                <select name="category" value={productData.category} onChange={handleChange} className="input-field">
+                    <option value="smartphone">SmartPhone</option>
+                    <option value="laptop">Laptop</option>
+                    <option value="watches">Watch</option>
+                </select>
 
                 <label htmlFor="price" className="input-label">Price:</label>
                 <input type="number" name="price" value={productData.price} onChange={handleChange}
-                       className="input-field"/>
+                       className="input-field" required />
 
-                <label htmlFor="discountedPrice" className="input-label">Discounted Price:</label>
+                <label htmlFor="discountedPrice" className="input-label">Discounted Percentage:</label>
                 <input type="number" name="discountPercentage" value={productData.discountPercentage}
-                       onChange={handleChange} className="input-field"/>
+                       onChange={handleChange} placeholder={"Write value between 1 to 100"} className="input-field"/>
+
 
                 <label htmlFor="quantity" className="input-label">Quantity:</label>
                 <input type="number" name="quantity" value={productData.quantity} onChange={handleChange}
-                       className="input-field"/>
+                       className="input-field" />
 
                 <label htmlFor="description" className="input-label">Description:</label>
                 <textarea name="description" value={productData.description} onChange={handleChange}
                           className="input-field"/>
 
-                <button type="button" onClick={handleSave}>Save</button>
+                <button type="submit">Save</button>
                 {successMessage ?? <div>{successMessage}</div>}
             </form>
 
-            <Link to="/custom-product">View Custom Products</Link>
         </div>
     );
 }
