5dacf557fa3b18c64481903f6b43609346ac42d9
Pulkit <pulkit.dadheech@noovosoft.com>
2023-11-02 15:53:56 +0530

    Add error message in form when quantity is zero

--- exercise/productlist-exercise/src/components/customProduct/CustomProductsForm.tsx
+++ exercise/productlist-exercise/src/components/customProduct/CustomProductsForm.tsx
@@ -7,12 +7,14 @@ import {CustomProductContext} from "../../CustomProductContext";
 function ProductForm() {
     const customProductContext = useContext(CustomProductContext);
     if (!customProductContext) {
-        throw new Error("UserContext is not provided correctly.");
+        throw new Error("CustomProductContext is not provided correctly.");
     }
+
     const {customProductId, setCustomProductId, customProducts, setCustomProducts} = customProductContext;
+
     const [productData, setProductData] = useState<TProductWithQuantity>({
         quantity: 0,
-        id: (customProductId),
+        id: customProductId,
         title: "",
         brand: "",
         category: "",
@@ -25,26 +27,33 @@ function ProductForm() {
         thumbnail: "",
         customProduct: true,
     });
+
     const [successMessage, setSuccessMessage] = useState<string>('');
     const [showSuccess, setShowSuccess] = useState(false);
-
+    const [showErrorMessage,setShowErrorMessage]=useState<string>("");
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
         const {name, value} = e.target;
         if (name === 'discountPercentage') {
             if (parseInt(value) > 100 || parseInt(value) < 0) {
-                return
+                return;
             }
         }
-        if(name === 'stock'){
-            setProductData({...productData,[name]:parseInt(value)})
-            return
+        if (name === 'stock') {
+            if(value === "0" ){
+                setShowErrorMessage("Value must be greater than zero");
+                setTimeout(()=>setShowErrorMessage(""),2000)
+                return
+            }
+            setProductData({...productData, [name]: parseInt(value)});
+            return;
         }
         setProductData({
             ...productData,
             [name]: value,
         });
     };
+
     useEffect(() => {
         localStorage.setItem("customId", JSON.stringify(customProductId));
         setCustomProductId(customProductId + 1);
@@ -52,11 +61,11 @@ function ProductForm() {
 
     const handleSave = (e: React.SyntheticEvent) => {
         e.preventDefault();
-        setCustomProductId(customProductId + 1)
+        setCustomProductId(customProductId + 1);
         setCustomProducts([...customProducts, productData]);
         setProductData({
             quantity: 0,
-            id: (customProductId),
+            id: customProductId,
             title: "",
             brand: "",
             category: "",
@@ -68,13 +77,12 @@ function ProductForm() {
             stock: productData.stock,
             thumbnail: "",
             customProduct: true,
-
         });
         setTimeout(() => setShowSuccess(false), 2000);
         setShowSuccess(true);
         setSuccessMessage('Submitted successfully!');
     };
-    console.log(productData.stock);
+
     return (
         <div className="form-container">
             <div className={"form-header"}>
@@ -83,12 +91,11 @@ function ProductForm() {
             </div>
             <form className="product-form" onSubmit={handleSave}>
                 <label htmlFor="name" className="input-label">Product Name:</label>
-                <input className="input-field" type="text" name="title" onChange={(e) => handleChange(e)}
+                <input className="input-field" type="text" name="title" onChange={handleChange}
                        value={productData.title} required/>
 
                 <label htmlFor="category" className="input-label">Category:</label>
-                <select name="category" value={productData.category} onChange={handleChange} className="input-field"
-                        required>
+                <select name="category"  value={productData.category} onChange={handleChange} className="input-field" required>
                     <option value="" disabled hidden>Select Category</option>
                     <option value="smartphone">SmartPhone</option>
                     <option value="laptop">Laptop</option>
@@ -96,29 +103,29 @@ function ProductForm() {
                 </select>
 
                 <label htmlFor="price" className="input-label">Price:</label>
-                <input type="number" name="price" value={productData.price} onChange={handleChange}
-                       className="input-field" required/>
+                <input type="number" name="price" onChange={handleChange}
+                       value={productData.price} className="input-field" required/>
 
-                <label htmlFor="discountedPrice" className="input-label">Discounted Percentage:</label>
+                <label htmlFor="discountedPercentage" className="input-label">Discounted Percentage:</label>
                 <input type="number" name="discountPercentage" value={productData.discountPercentage}
-                       onChange={handleChange} placeholder={"Write value between 1 to 100"} className="input-field"/>
+                       onChange={handleChange} placeholder="Write value between 1 to 100" className="input-field"/>
 
                 <label htmlFor="stock" className="input-label">Quantity:</label>
                 <div className="product-form-stock">
-                    <button type="button" onClick={() =>setProductData({...productData,"stock": productData.stock+1})}>+</button>
-                    <input type="number" name="stock" value={productData.stock}
-                           onChange={handleChange} className="input-field"/>
-                    <button type="button" onClick={() =>setProductData({...productData,"stock": productData.stock!==0? productData.stock-1: 0})}>-</button>
+                    <button type="button" onClick={() => setProductData({ ...productData, stock: productData.stock + 1 })}>+</button>
+                    <input type="number" name="stock" onChange={handleChange}
+                        value={productData.stock} className="input-field"/>
+                    <button type="button" onClick={() => setProductData({ ...productData, stock: productData.stock !==1  ? productData.stock - 1 : 1 })}>-</button>
                 </div>
+                <p className="error-message">{showErrorMessage}</p>
 
                 <label htmlFor="description" className="input-label">Description:</label>
-                <textarea name="description" value={productData.description} onChange={handleChange}
-                          className="input-field"/>
+                <textarea name="description" value={productData.description}
+                    onChange={handleChange} className="input-field"/>
 
                 <button type="submit">Save</button>
                 {showSuccess && <div className="success-message">{successMessage}</div>}
             </form>
-
         </div>
     );
 }
