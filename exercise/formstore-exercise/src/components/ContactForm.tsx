f9be40036540daeb214e1908cc078feb8aae50cb
Pulkit <pulkit.dadheech@noovosoft.com>
2023-11-17 17:18:04 +0530

    add Contact Form

---
+++ exercise/formstore-exercise/src/components/ContactForm.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import {observer} from "mobx-react-lite";
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {FormStore} from "../store/FormStore";
+
+export interface IContactFormProps {
+    name: string;
+    email: string;
+    message: string;
+}
+
+const contactFormStore = new FormStore<IContactFormProps>({
+    name: "",
+    email: "",
+    message: "",
+});
+
+export const ContactForm = observer(() => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        contactFormStore.updateFormData(name as keyof IContactFormProps, value);
+    };
+
+
+    return (
+        <div className="Register-Form">
+            <h2>Sign In</h2>
+            <Form className="form">
+                <FormGroup>
+                    <Label
+                        for="exampleName"
+                    >Name
+                    </Label>
+                    <Input
+                        type={"text"}
+                        name={"name"}
+                        placeholder={"Name"}
+                        value={contactFormStore.getValue('name')}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label
+                        for="exampleEmail"
+                    >
+                        Email
+                    </Label>
+                    <Input
+                        id="exampleEmail"
+                        type={"email"}
+                        name={"email"}
+                        placeholder={"Email"}
+                        value={contactFormStore.getValue('email')}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <FormGroup>
+                    <Label
+                        for="examplePassword"
+                    >
+                        Password
+                    </Label>
+                    <Input
+                        id="examplePassword"
+                        type={"textarea"}
+                        name={"textarea"}
+                        placeholder={"Message"}
+                        value={contactFormStore.getValue('message')}
+                        onChange={handleChange}
+                    />
+                </FormGroup>
+                <Button>Submit</Button>
+            </Form>
+        </div>
+    )
+});
