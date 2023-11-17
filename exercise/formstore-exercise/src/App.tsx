7a7108884a27b708e1ec1139584e51ccc59da1db
Pulkit <pulkit.dadheech@noovosoft.com>
2023-11-17 16:15:12 +0530

    Add entities for register form

--- exercise/formstore-exercise/src/App.tsx
+++ exercise/formstore-exercise/src/App.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
+import {RegisterForm} from "./components/RegisterForm";
+import {FormStore} from "./store/FormStore";
+
+export interface IRegisterFormProps {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export const registerFormStore = new FormStore<IRegisterFormProps>({
+    username: "",
+    email: "",
+    password: "",
+});
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <RegisterForm RegisterForm={registerFormStore} />
+        </div>
+    );
 }
 
 export default App;
