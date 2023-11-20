bace0f1a6589023a95ea41a9a046b4c746072d68
Pulkit <pulkit.dadheech@noovosoft.com>
2023-11-20 13:42:12 +0530

    Add decorator in FormStore.tsx

--- exercise/formstore-exercise/src/store/FormStore.tsx
+++ exercise/formstore-exercise/src/store/FormStore.tsx
@@ -1,21 +1,20 @@
 import {action, makeObservable, observable} from "mobx";
 
 export class FormStore<T> {
-    formData: T;
+    @observable formData: T;
 
     constructor(initialData: T) {
         this.formData = initialData;
-        makeObservable(this, {
-            formData: observable,
-            updateFormData: action,
-        });
+        makeObservable(this);
     }
 
     getValue(entity: keyof T) {
-        return this.formData[entity]
+        return this.formData[entity];
     }
 
+    @action
     updateFormData<K extends keyof T>(entity: K, value: T[K]) {
         this.formData[entity] = value;
     }
+
 }
