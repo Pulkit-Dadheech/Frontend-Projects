f1de92a3b40df2dab6233bc8869e80eb2562e5cb
Pulkit <pulkit.dadheech@noovosoft.com>
2023-12-27 14:02:20 +0530

    update ListTableStore.tsx for adding pagination feature

--- exercise/list-table-store/src/store/ListTableStore.tsx
+++ exercise/list-table-store/src/store/ListTableStore.tsx
@@ -1,10 +1,32 @@
-import {makeObservable, observable} from "mobx";
+import {action, makeObservable, observable} from "mobx";
 
 export class ListTableStore<T> {
-    @observable data: T | undefined;
+    @observable data: T;
+    @observable skip: number=0;
+    total: number=0;
 
-    constructor(promiseData: any) {
+    constructor(promiseData: T) {
         this.data = promiseData;
         makeObservable(this);
+    }
+
+    getData() {
+        return this.data;
+    }
+
+    nextPage(){
+       if(this.skip<this.total-10){
+           this.skip=this.skip+10;
+       }
+       console.log(this.skip,this.total);
+    }
+
+    prevPage(){
+        if(this.skip>=10)
+        this.skip=this.skip-10;
+    }
+
+    @action updateData(data: T){
+        this.data=data;
     }
 }
