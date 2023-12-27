1795f4321fb40903ce1c8a8fe2cfdbc72f26e0da
Pulkit <pulkit.dadheech@noovosoft.com>
2023-12-27 13:02:11 +0530

    created list-table-store for performing form-store-task-four

---
+++ exercise/list-table-store/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import {TableStoreComponent} from "./component/tableStoreComponent";
+
+function App() {
+    return (
+        <>
+            <TableStoreComponent/>
+        </>
+    );
+}
+
+export default App;
