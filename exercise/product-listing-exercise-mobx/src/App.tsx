30bec147e167012835e3ff2e593421331bafd385
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-03 12:13:36 +0530

    created new product-listing-exercise-mobx

---
+++ exercise/product-listing-exercise-mobx/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {initRouter} from "./initRouter";
+import {RouterContext, RouterView} from 'mobx-state-router';
+import {viewMap} from "./components/viewMap";
+
+export default function App() {
+    const routerStore = initRouter();
+
+    return (
+        <RouterContext.Provider value={routerStore}>
+            <RouterView viewMap={viewMap}/>
+        </RouterContext.Provider>
+    );
+}
