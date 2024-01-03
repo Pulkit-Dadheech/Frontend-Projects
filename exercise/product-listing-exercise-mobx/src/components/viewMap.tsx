701c035b880c63b1ede4cd28b41feac6bb23b5ad
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-03 12:31:29 +0530

    added mobx route and its initRouter and viewMap files for routing

---
+++ exercise/product-listing-exercise-mobx/src/components/viewMap.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import HomePage from "./Homepage/HomePage";
+import NoResultFound from "./NoSearchResultFound/NoResultFound";
+
+export const viewMap = {
+    home: <HomePage />,
+    notFound: <NoResultFound />,
+};
