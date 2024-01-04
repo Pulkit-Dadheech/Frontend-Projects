5ea350f1eab586433341964877c30eed4175ba79
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-04 17:07:17 +0530

    created new RootStore and its context to initialize all the store in a single file

---
+++ exercise/product-listing-exercise-mobx/src/store/RootStore.ts
@@ -0,0 +1,18 @@
+import {ProductStore} from "./ProductStore";
+import {CartStore} from "./cartStore";
+import {UsersStore} from "./usersStore";
+import {CategoryStore} from "./CategoryStore";
+
+export class RootStore  {
+    product: ProductStore
+    cart: CartStore
+    users: UsersStore
+    category: CategoryStore
+
+    constructor() {
+        this.product=new ProductStore();
+        this.cart=new CartStore();
+        this.users=new UsersStore();
+        this.category=new CategoryStore();
+    }
+};
--- exercise/product-listing-exercise-mobx/src/viewMap.tsx
+++ exercise/product-listing-exercise-mobx/src/viewMap.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import HomePage from "./components/Homepage/HomePage";
 import NoResultFound from "./components/NoSearchResultFound/NoResultFound";
+import {CartPage} from "./components/cartPage/CartPage";
 
 export const viewMap = {
-    home: <HomePage />,
-    notFound: <NoResultFound />,
+    home: <HomePage/>,
+    cart: <CartPage/>,
+    notFound: <NoResultFound/>,
 };
