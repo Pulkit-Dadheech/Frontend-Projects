0571a13950de0a02af1a6a1a5c3f2aa4bd23d445
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-05 13:35:09 +0530

    update CustomCartHeader

--- exercise/product-listing-exercise-mobx/src/components/customProduct/CustomCart/CustomCartHeader.tsx
+++ exercise/product-listing-exercise-mobx/src/components/customProduct/CustomCart/CustomCartHeader.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import "../CustomProductMainPage/CustomHeader.css"
 import {useRouterStore} from "mobx-state-router";
 
-export function CustomCartHeader() {
+export const CustomCartHeader=()=> {
     const routerStore = useRouterStore();
     return (
         <>
             <div className={"custom-header-elements"}>
-                <h1>Custom Products</h1>
+                <h1>Custom Cart</h1>
                 <div id="custom-form">
                     <button onClick={() => routerStore.goTo('customForm')}>Add Product</button>
                 </div>
                 <div id="cart">
-                    <button onClick={() => routerStore.goTo('customProduct')}>Go to Cart</button>
+                    <button onClick={() => routerStore.goTo('customProduct')}>Go to Product Page</button>
                 </div>
             </div>
         </>
