5a75f682d8999e37dc02f2de06b2774defbe4c39
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-05 11:12:34 +0530

    created a new CustomProductStore for storing CustomProducts Data

---
+++ exercise/product-listing-exercise-mobx/src/store/CustomProductStore.ts
@@ -0,0 +1,35 @@
+import {action, makeObservable, observable} from "mobx";
+import {ListTableStore} from "./ListTableStore";
+import {TCustomProduct, TSingleCustomProduct} from "../types/allTypes";
+export class CustomProductStore {
+    customProductStore: ListTableStore<TCustomProduct>;
+    customProductId: number=1;
+    @observable customProductData: TSingleCustomProduct = {id: 0, title: "", category: "", quantity: 0, description: "", discountPercentage: 0, images: [], price: 0, rating: 4.5, total: 1, customProduct: true};
+
+    constructor() {
+        this.customProductStore = new ListTableStore<TCustomProduct>(this.fetchData);
+        makeObservable(this);
+    }
+
+    fetchData() {
+        const data: TCustomProduct = []
+        return data;
+    }
+
+    @observable setCustomProductData(data: TSingleCustomProduct) {
+        this.customProductData = data;
+    }
+
+    getCustomProductEntityData(entity: keyof TSingleCustomProduct) {
+        return this.customProductData[entity];
+    }
+
+    @action
+    updateCustomProductSingleEntityData<K extends keyof TSingleCustomProduct>(entity: K, value: TSingleCustomProduct[K]) {
+        this.customProductData[entity] = value;
+    }
+
+}
+
+
+
