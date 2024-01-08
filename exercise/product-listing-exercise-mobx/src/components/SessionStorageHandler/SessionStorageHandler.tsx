b8a174696decbf9474a2e183a689f800ee222106
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-08 16:03:49 +0530

    used localStorage instead of session storage

--- exercise/product-listing-exercise-mobx/src/components/SessionStorageHandler/SessionStorageHandler.tsx
+++ exercise/product-listing-exercise-mobx/src/components/SessionStorageHandler/SessionStorageHandler.tsx
@@ -1,10 +1,10 @@
 export function SessionStorageSetter(name: string,data: any) {
-    sessionStorage.setItem(name, JSON.stringify(data));
+    localStorage.setItem(name, JSON.stringify(data));
 
 }
 
 export function SessionStorageGetter(name: string) {
-    const data=sessionStorage.getItem(name);
+    const data=localStorage.getItem(name);
     if(data){
         return JSON.parse(data);
     }
