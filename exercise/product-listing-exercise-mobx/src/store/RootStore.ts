d4c450dd41368c290ad2d3a372119dc2321535b0
Pulkit <pulkit.dadheech@noovosoft.com>
2024-01-09 12:46:58 +0530

    remove unnecessary formStore from RootStore.ts

--- exercise/product-listing-exercise-mobx/src/store/RootStore.ts
+++ exercise/product-listing-exercise-mobx/src/store/RootStore.ts
@@ -2,30 +2,17 @@ import {ProductStore} from "./ProductStore";
 import {CartStore} from "./cartStore";
 import {UsersStore} from "./usersStore";
 import {CategoryStore} from "./CategoryStore";
-import {FormStore} from "./FormStore";
-import {IFormProps} from "../components/customProduct/CustomProductForm/CustomProductsForm";
 
-export class RootStore  {
+export class RootStore {
     product: ProductStore
     cart: CartStore
     users: UsersStore
     category: CategoryStore
-    formStore: FormStore<IFormProps>;
 
     constructor() {
-        this.product=new ProductStore();
-        this.cart=new CartStore();
-        this.users=new UsersStore();
-        this.category=new CategoryStore();
-        this.formStore = new FormStore<IFormProps>({
-            title: {value: "",isRequired: true},
-            category: {value: "", isRequired: true},
-            price: {value: 0, isRequired: true},
-            discountPercentage: {value: 0, isRequired: true},
-            total: {value: 1, isRequired: true},
-            description: {value: ""},
-        });
-
-
+        this.product = new ProductStore();
+        this.cart = new CartStore();
+        this.users = new UsersStore();
+        this.category = new CategoryStore();
     }
 }
